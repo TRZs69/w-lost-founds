af3e6825865d95ec15bc9e256caabebf8a729482
viktrislubis <kristrianiviktris@gmail.com>
2024-10-18 23:12:18 +0700

    memperbaharui RegisterPage.jsx dan RegisterInput.jsx

--- src/components/RegisterInput.jsx
+++ src/components/RegisterInput.jsx
@@ -7,58 +7,57 @@ function RegisterInput({ onAuthRegister }) {
   const [password, onPasswordChange] = useInput("");
 
   const handleSubmit = (e) => {
-    e.preventDefault(); // Mencegah refresh halaman
+    e.preventDefault(); // Prevent page refresh
     onAuthRegister({ name, email, password });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="form px-4">
-        <div className="mb-3">
-          <label htmlFor="inputName" className="form-label">
-            Full Name
-          </label>
-          <input
-            type="text"
-            id="inputName"
-            value={name}
-            onChange={onNameChange}
-            className="form-control"
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="inputEmail" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            id="inputEmail"
-            value={email}
-            onChange={onEmailChange}
-            className="form-control"
-            placeholder="email@example.com"
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="inputPassword" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            id="inputPassword"
-            value={password}
-            onChange={onPasswordChange}
-            className="form-control"
-            required
-          />
-        </div>
-        <div className="mb-4 text-end">
-          <button type="submit" className="btn btn-primary">
-            Register
-          </button>
-        </div>
+    <form onSubmit={handleSubmit} className="register-form">
+      <h1>Register Now!</h1>
+      <div className="mb-3">
+        <label htmlFor="inputName" className="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          id="inputName"
+          value={name}
+          onChange={onNameChange}
+          className="form-control"
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="inputEmail" className="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          id="inputEmail"
+          value={email}
+          onChange={onEmailChange}
+          className="form-control"
+          placeholder="email@example.com"
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="inputPassword" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          id="inputPassword"
+          value={password}
+          onChange={onPasswordChange}
+          className="form-control"
+          required
+        />
+      </div>
+      <div className="mb-4 text-end">
+        <button type="submit" className="btn btn-signup">
+          Sign Up
+        </button>
       </div>
     </form>
   );
