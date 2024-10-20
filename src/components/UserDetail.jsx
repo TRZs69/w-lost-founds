5fde298baa98c1dfc539ab24c704ede0f69ab07f
viktrislubis <kristrianiviktris@gmail.com>
2024-10-20 15:41:18 +0700

    memperbaharui User Detail

--- src/components/UserDetail.jsx
+++ src/components/UserDetail.jsx
@@ -18,56 +18,49 @@ function UserDetail({ authLogin, onUserChangePhoto }) {
   };
 
   return (
-    <div id={authLogin.id} className="card">
-      <div className="card-body">
-        <div className="row align-items-center">
-          <div className="col-12">
-            <div className="d-flex">
-              <div>
-                <img
-                  className="rounded-circle"
-                  width={76}
-                  height={76}
-                  src={authLogin.photo}
-                />
-              </div>
-              <div className="ms-3">
-                <h3 className="text-primary">{authLogin.name}</h3>
+    <div id={authLogin.id} className="card profile-container">
+      <div className="card-body text-center">
+        <div className="d-flex flex-column align-items-center">
+          <img
+            className="rounded-circle"
+            width={100}
+            height={100}
+            src={authLogin.photo}
+            alt="Profile"
+          />
+          <h3 className="text-primary mt-3">{authLogin.name}</h3>
 
-                <button
-                  className="btn btn-sm btn-outline-primary"
-                  onClick={handleUploadClick}
-                >
-                  <FaUpload /> Ubah Photo Profile
-                </button>
-                <input
-                  ref={fileInputRef}
-                  type="file"
-                  className="d-none"
-                  accept="image/*"
-                  onChange={handleFileChange}
-                />
-              </div>
-            </div>
-            <hr />
-            <table className="table table-bordered">
-              <tbody>
-                <tr>
-                  <th>Nama</th>
-                  <td>{authLogin.name}</td>
-                </tr>
-                <tr>
-                  <th>Email</th>
-                  <td>{authLogin.email}</td>
-                </tr>
-                <tr>
-                  <th>Bergabung sejak</th>
-                  <td>{formatDate(authLogin.created_at)}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <button
+            className="btn btn-sm btn-outline-primary mt-2"
+            onClick={handleUploadClick}
+          >
+            <FaUpload /> Ubah Photo Profile
+          </button>
+          <input
+            ref={fileInputRef}
+            type="file"
+            className="d-none"
+            accept="image/*"
+            onChange={handleFileChange}
+          />
         </div>
+        <hr />
+        <table className="table table-bordered profile-table">
+          <tbody>
+            <tr>
+              <th>Nama</th>
+              <td>{authLogin.name}</td>
+            </tr>
+            <tr>
+              <th>Email</th>
+              <td>{authLogin.email}</td>
+            </tr>
+            <tr>
+              <th>Bergabung sejak</th>
+              <td>{formatDate(authLogin.created_at)}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
